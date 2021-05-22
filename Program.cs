6e9821012fbd610d48915ded413007b621c6e43d
ogulyukin <o.gulyukin@yandex.ru>
2021-05-22 20:44:11 +0200

    02 Done

--- Program.cs
+++ Program.cs
@@ -5,14 +5,29 @@ namespace HWork01
 {
     class Program
     {
-        static void MyTask()
+        static void MyTask(int begin, int end)
         {
             Console.WriteLine("Starting addition thread...");
-            for (int i = 0; i <= 50; i++)
+            for (int i = begin; i <= end; i++)
             {
                 Console.WriteLine($"Now is: {i}");
             }
             Console.WriteLine("Addition thread Done!!!");
+        }
+
+        static int getUserInput()
+        {
+            int result = 0;
+            try
+            {
+                result = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number was entered!!! Turn it to Zero");
+                return 0;
+            }
+            return result;
         }
         static void Main(string[] args)
         {
@@ -20,7 +35,12 @@ namespace HWork01
             Console.WriteLine("        This program show basics use of Multithreading.");
             Console.WriteLine("***************************************************************");
             Console.WriteLine("Starting Main thread...");
-            Task task01 = new Task(MyTask);
+            int begin, end;
+            Console.WriteLine("Enter begin number:");
+            begin = getUserInput();
+            Console.WriteLine("Enter end number:");
+            end = getUserInput();
+            Task task01 = new Task(() => MyTask(begin, end));
             task01.Start();
             Console.WriteLine("Ending Main thread...");
             task01.Wait();
